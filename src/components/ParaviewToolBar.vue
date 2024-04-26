4315c95c49b90567bdeefb323986e49dc58c94d0
Amy <amy.huang@lyncustek.com>
2024-04-26 10:20:41 +0000

    Move toolbar to an independent component

---
+++ src/components/ParaviewToolBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar class="px-4" app>
+    <span class="title">Trame</span>
+    <v-spacer />
+
+    <!-- Reset camera -->
+    <v-btn color="primary" icon="$slice" @click="slice"></v-btn>
+    <v-btn color="primary" icon="mdi-camera" @click="resetCamera"></v-btn>
+
+    <v-progress-linear
+      :active="!!busy"
+      :indeterminate="!!busy"
+      :absolute="true"
+      style="bottom: 0"
+    />
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useWSLinkStore, useFilterStore } from '@/store'
+
+/* -- store -- */
+const wslinkStore = useWSLinkStore()
+const filterStore = useFilterStore()
+
+/* -- data -- */
+const { busy } = storeToRefs(wslinkStore)
+
+/* -- method -- */
+const { resetCamera } = wslinkStore
+const { slice } = filterStore
+</script>
--- src/views/Paraview.vue
+++ src/views/Paraview.vue
@@ -1,32 +1,9 @@
 <template>
   <div class="fill-height overflow-hidden">
-    <v-app-bar app>
-      <span class="ms-4 title">Application</span>
-      <v-spacer />
-
-      <!-- [Custom Method] Change resolution of the cone (not actually implement on server side) -->
-      <v-slider
-        hide-details
-        :model-value="resolution"
-        :max="60"
-        :min="3"
-        :step="1"
-        @update:model-value="updateConeResolution"
-      />
-
-      <!-- [Custom Method] Reset camera (not actually implement on server side) -->
-      <v-icon icon="mdi-camera" class="mx-4" @click="resetCamera" />
-
-      <v-progress-linear
-        :active="!!busy"
-        :indeterminate="!!busy"
-        :absolute="true"
-        style="bottom: 0"
-      />
-    </v-app-bar>
+    <paraview-tool-bar />
 
     <div style="position: relative; width: 100%; height: 100%">
-      <remote-rendering-view :client="client" />
+      <remote-rendering-view />
     </div>
   </div>
 </template>
@@ -34,11 +11,11 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue'
 
-import { storeToRefs } from 'pinia'
-import { useWSLinkStore, useConeStore } from '@/store'
+import { useWSLinkStore } from '@/store'
 
 import { useRoute } from 'vue-router'
 
+import ParaviewToolBar from '@/components/ParaviewToolBar.vue'
 import RemoteRenderingView from '@/components/RemoteRenderingView.vue'
 
 /* -- route -- */
@@ -46,15 +23,9 @@ const route = useRoute()
 
 /* -- store -- */
 const wslinkStore = useWSLinkStore()
-const coneStore = useConeStore()
-
-/* -- data -- */
-const { client, busy } = storeToRefs(wslinkStore)
-const { resolution } = storeToRefs(coneStore)
 
 /* -- method -- */
-const { updateConeResolution } = coneStore
-const { connect, resetCamera } = wslinkStore
+const { connect } = wslinkStore
 
 /* -- lifecycle -- */
 onMounted(() => {
